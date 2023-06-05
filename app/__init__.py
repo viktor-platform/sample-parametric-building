80ab59808e4999fd6fd4d0b85211b4397a79db4d
LuukBoot <121462516+LuukBoot@users.noreply.github.com>
2023-06-05 11:58:49 +0200

    Updated

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,10 @@ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE A
 THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-from .building.controller import BuildingController
+from .building.controller import BuildingController as Building
+
+from viktor import InitialEntity
+
+initial_entities = [
+    InitialEntity('Building', name='Parametric Building Structure')
+]
